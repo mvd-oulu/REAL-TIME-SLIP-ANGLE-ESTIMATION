b09bc09480a17ac9eaf85f51dfc8057188d9013f
Shalika Dulaj Amarathunga <58818511+shalikadulaj@users.noreply.github.com>
2025-05-07 09:48:28 +0300

    Update and rename Real-time_Slip_Angle_Estimation_Main.ino to Real-time_Slip_Angle_Estimation_OnlyForDevelopedAlgorithm.ino

--- Real-time_Slip_Angle_Estimation_OnlyForDevelopedAlgorithm.ino
+++ Real-time_Slip_Angle_Estimation_OnlyForDevelopedAlgorithm.ino
@@ -552,28 +552,7 @@ float convertToDecimalDegrees(float degMin) {
   return decimalDegrees;
 }
 
-void relPosDecode() {
 
-  int carrSoln;
-  bool gnssFixOk, diffSoln, relPosValid;
-
-  heading = (int32_t)ackPacket[30] + ((int32_t)ackPacket[31] << 8)
-            + ((int32_t)ackPacket[32] << 16) + ((int32_t)ackPacket[33] << 24);
-  heading *= 0.0001;
-
-  heading += headingcorr;
-  if (heading >= 3600) heading -= 3600;
-  if (heading < 0) heading += 3600;
-  heading *= 0.1;
-
-  baseline = (int32_t)ackPacket[26] + ((int32_t)ackPacket[27] << 8)
-             + ((int32_t)ackPacket[28] << 16) + ((int32_t)ackPacket[29] << 24);
-  double baseHP = (signed char)ackPacket[41];
-  baseHP *= 0.01;
-  baseline += baseHP;
-
-  relPosD = (int32_t)ackPacket[22] + ((int32_t)ackPacket[23] << 8)
-            + ((int32_t)ackPacket[24] << 16) + ((int32_t)ackPacket[25] << 24);
   double relPosHPD = (signed char)ackPacket[40];
   relPosHPD *= 0.01;
   relPosD += relPosHPD;
